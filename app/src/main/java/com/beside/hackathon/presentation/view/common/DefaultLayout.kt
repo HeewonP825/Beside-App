65bc5f7c01cf6dc1e6dd1b9b9b07a8c0ea16514b
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-11 12:32:27 +0900

    [Feat]: DefaultLayout 뒤로가기 버튼 추가

--- app/src/main/java/com/beside/hackathon/presentation/view/common/DefaultLayout.kt
+++ app/src/main/java/com/beside/hackathon/presentation/view/common/DefaultLayout.kt
@@ -2,13 +2,19 @@ package com.beside.hackathon.presentation.view.common
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.beside.hackathon.core.utils.TextSyles.TITLE_TEXT_STYLE
 
@@ -17,20 +23,34 @@ import com.beside.hackathon.core.utils.TextSyles.TITLE_TEXT_STYLE
 @Composable
 fun DefaultLayout(
     title: String? = null,
+    backButtonOnClick: () -> Unit = {},
     body: @Composable () -> Unit,
 ){
     Scaffold (
         topBar={
             title?.let {
                 TopAppBar(
+
+                    actions = {
+
+                    },
                     title = {
-                        Text(text = title, style = TITLE_TEXT_STYLE)
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            IconButton(onClick = backButtonOnClick) {
+                                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                            }
+                            Text(text = title, style = TITLE_TEXT_STYLE)
+                        }
                     }
                 )
             }
         }
     ){
-        Box(modifier = Modifier.padding(it).fillMaxSize()) {
+        Box(modifier = Modifier
+            .padding(it)
+            .fillMaxSize()) {
             body()
         }
     }
