a0b7b054e9b2dac0ad63441b9f7feb822fd68ba5
HeewonP825 <7hwon@naver.com>
2023-11-11 22:46:05 +0900

    Design(LoginScreen): 회원가입문구 오타수정

--- app/src/main/java/com/beside/hackathon/presentation/view/login/LoginScreen.kt
+++ app/src/main/java/com/beside/hackathon/presentation/view/login/LoginScreen.kt
@@ -173,7 +173,7 @@ internal fun BottomPart(
         horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Text("아직 계정 없으신가요? 회원가입",style =CONTENT_TEXT3_STYLE.copy(
+        Text("아직 계정이 없으신가요? 회원가입",style =CONTENT_TEXT3_STYLE.copy(
             color = Color(0xFF707070)
         ),
             modifier = Modifier.clickable {
