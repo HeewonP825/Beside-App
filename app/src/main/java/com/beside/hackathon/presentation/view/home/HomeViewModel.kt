894d94dff3c9cd44643fe001639fa27645778cbb
HeewonP825 <7hwon@naver.com>
2023-11-11 18:39:15 +0900

    Feat(homeFragment): 각 랭킹별로 어댑터 구현 완료 및 예시데이터 삽입

--- app/src/main/java/com/beside/hackathon/presentation/view/home/HomeViewModel.kt
+++ app/src/main/java/com/beside/hackathon/presentation/view/home/HomeViewModel.kt
@@ -3,12 +3,35 @@ package com.beside.hackathon.presentation.view.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.beside.hackathon.data.model.home.Ranking
 
 class HomeViewModel : ViewModel() {
-    private val _currentPage = MutableLiveData<Int>()
-    val currentPage: LiveData<Int> get() = _currentPage
 
+    // 현재 페이지를 저장하는 MutableLiveData
+    private val _currentPage = MutableLiveData<Int>()
+    val currentPage: LiveData<Int> = _currentPage
+
+    private val _dataForTotalRankingFragment = MutableLiveData<List<Ranking>>()
+    val dataForTotalRankingFragment: LiveData<List<Ranking>> = _dataForTotalRankingFragment
+
+    private val _dataForUnivRankingFragment = MutableLiveData<List<Ranking>>()
+    val dataForUnivRankingFragment: LiveData<List<Ranking>> = _dataForUnivRankingFragment
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        // 예시 데이터
+        _dataForTotalRankingFragment.value = listOf(/* ... 데이터 ... */)
+        _dataForUnivRankingFragment.value = listOf(/* ... 데이터 ... */)
+    }
+
+    // 현재 페이지를 설정하는 메서드
     fun setCurrentPage(page: Int) {
         _currentPage.value = page
     }
+
+    // 필요한 경우 추가 데이터 로드 및 처리 로직
 }
+
--- app/src/main/java/com/beside/hackathon/presentation/view/home/TotalRankingFragment.kt
+++ app/src/main/java/com/beside/hackathon/presentation/view/home/TotalRankingFragment.kt
@@ -6,11 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.beside.hackathon.R
+import com.beside.hackathon.data.model.home.Ranking
 import com.beside.hackathon.databinding.FragmentHomeBinding
 import com.beside.hackathon.databinding.FragmentTotalRankingBinding
 
@@ -18,6 +22,10 @@ class TotalRankingFragment : Fragment() {
     private var _binding: FragmentTotalRankingBinding? = null
     private lateinit var navController: NavController
     private val binding get() = _binding!!
+
+    private lateinit var totalRankingAdapter: TotalRankingAdapter
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +36,37 @@ class TotalRankingFragment : Fragment() {
         navController = findNavController()
 
         _binding = FragmentTotalRankingBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        // RecyclerView 초기화
+        recyclerView = binding.totalRankingRv
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // 예시 데이터 생성
+        val exampleData = listOf(
+            Ranking(6, "000", "경북대학교", 78),
+            Ranking(1, "000", "경북대학교", 98),
+            Ranking(2, "000", "경북대학교", 88),
+            Ranking(3, "000", "경북대학교", 84),
+            // ... 추가 데이터 ...
+        )
+
+        // 어댑터 설정
+        totalRankingAdapter = TotalRankingAdapter(exampleData) { item ->
+            // 아이템 클릭 시 수행할 작업
+        }
+        recyclerView.adapter = totalRankingAdapter
+
+//        recyclerView = binding.totalRankingRv // RecyclerView ID에 맞게 변경
+//        recyclerView.layoutManager = LinearLayoutManager(context)
+//
+//        viewModel.dataForTotalRankingFragment.observe(viewLifecycleOwner, Observer { data ->
+//            totalRankingAdapter = TotalRankingAdapter(data) { item ->
+//                // 아이템 클릭 시 수행할 작업
+//            }
+//            recyclerView.adapter = totalRankingAdapter
+//        })
+
 
         val root: View = binding.root
 
