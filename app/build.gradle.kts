9a91f86f4ef563eef3dae0ca658c2af3d2d33ad3
HeewonP825 <7hwon@naver.com>
2023-11-11 22:18:02 +0900

    Chore(Firebase): Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     buildFeatures {
         compose = true
         viewBinding = true
+        dataBinding = true
     }
     kotlinOptions {
         jvmTarget = "17"
@@ -57,6 +58,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.5")
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    implementation("androidx.databinding:databinding-runtime:7.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -110,6 +113,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-messaging-ktx:23.3.1")
 
 
 }
--- app/src/main/java/com/beside/hackathon/MainActivity.kt
+++ app/src/main/java/com/beside/hackathon/MainActivity.kt
@@ -2,14 +2,20 @@ package com.beside.hackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.beside.hackathon.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -35,6 +41,23 @@ class MainActivity : AppCompatActivity(), ViewTreeObserver.OnPreDrawListener {
         with(WindowInsetsControllerCompat(window, window.decorView)) {
             isAppearanceLightStatusBars = true
         }
+
+        Firebase.messaging.isAutoInitEnabled = true
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = token.toString()
+            Log.d("FCM", msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
