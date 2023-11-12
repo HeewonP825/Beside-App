42b006f1eeabd2aa09e35f519266c624123943dc
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-12 11:44:54 +0900

    [Feat]: ip 변경

--- app/src/main/java/com/beside/hackathon/core/utils/Constant.kt
+++ app/src/main/java/com/beside/hackathon/core/utils/Constant.kt
@@ -3,7 +3,7 @@ package com.beside.hackathon.core.utils
 import androidx.compose.ui.unit.dp
 
 object Constant{
-    const val BASE_URL = "http://googel.com"
+    const val BASE_URL = "http://13.209.102.36:8080"
     val BORDER_RADIUS = 15.dp
     val DEFAULT_PADDING_V = 20.dp
     val DEFAULT_PADDING_H = 20.dp
