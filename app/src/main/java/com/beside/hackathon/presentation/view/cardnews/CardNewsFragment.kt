51480fcca3c5f22de771a4eca043b2c7e8e3afaf
HeewonP825 <7hwon@naver.com>
2023-11-12 14:01:12 +0900

    Feat(CardNewsFragment): 버튼 스크롤 반응형으로 구현 완료

--- app/src/main/java/com/beside/hackathon/presentation/view/cardnews/CardNewsFragment.kt
+++ app/src/main/java/com/beside/hackathon/presentation/view/cardnews/CardNewsFragment.kt
@@ -1,10 +1,13 @@
 package com.beside.hackathon.presentation.view.cardnews
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -57,5 +60,30 @@ class CardNewsFragment : Fragment() {
         // 데이터 로드
         cardNewsViewModel.loadCardNewsUrls()
 
+        binding.cardnewsRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                try {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                    val totalItemCount = layoutManager.itemCount
+                    val lastVisible = layoutManager.findLastCompletelyVisibleItemPosition()
+
+                    val endHasBeenReached = lastVisible + 1 >= totalItemCount
+                    if (totalItemCount > 0 && endHasBeenReached) {
+                        // 스크롤이 끝에 도달했을 때 버튼 상태 변경
+                        binding.readBtn.apply {
+                            isEnabled = true
+                            //setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.bg_cardnews_read_btn))// 색상 변경
+                            setBackgroundColor(ContextCompat.getColor(context, R.color.mainColor)) // 색상 변경
+                        }
+                        binding.readBtn.setOnClickListener {
+                            findNavController().navigate(R.id.action_cardNewsFragment_to_homeFragment)
+                        }
+                    }
+                } catch (e: Exception) {
+                    Log.e("CardNewsFragment", "Scroll error: ${e.message}", e)
+                }
+            }
+        })
     }
 }
