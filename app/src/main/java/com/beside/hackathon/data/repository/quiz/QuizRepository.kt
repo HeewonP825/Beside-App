9fc8bc3342cdef020f890d3f98704cdc4ce85837
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-12 21:23:33 +0900

    [Design]: 오늘의 퀴즈 api 연동

--- app/src/main/java/com/beside/hackathon/data/repository/quiz/QuizRepository.kt
+++ app/src/main/java/com/beside/hackathon/data/repository/quiz/QuizRepository.kt
@@ -14,28 +14,8 @@ class QuizRepository @Inject constructor(
 
 
     suspend fun getTodayQuiz(): Quiz {
-        return Quiz(
-            1,"테스트 퀴즈명", listOf(
-                Question(
-                    1,"테스트 질문1", listOf(
-                        Option(1,"테스트 옵션1"),
-                        Option(2,"테스트 옵션2"),
-                        Option(3,"테스트 옵션3"),
-                        Option(4,"테스트 옵션4"),
-                    )
-                ),
-                Question(
-                    2,"테스트 질문2", listOf(
-                        Option(5,"테스트 옵션5"),
-                        Option(6,"테스트 옵션6"),
-                        Option(7,"테스트 옵션7"),
-                        Option(8,"테스트 옵션8"),
-                    )
-                ),
-            )
-        )
 
-        //return apiService.getTodayQuiz()
+        return apiService.getTodayQuiz()
     }
     suspend fun submitQuiz(
         selectedOptionIds: List<Int>,
